02e63e865404efa47435d1349404979daee7b0ac
cal20014 <cal20014@byui.edu>
2024-03-16 19:49:21 -0600

    Added PUT and DELETE functions

--- controllers/kyberDataController.js
+++ controllers/kyberDataController.js
@@ -26,48 +26,48 @@ kyberDataController.getKyberDataById = async (req, res) => {
   }
 };
 
-// // POST new kyberData
-// kyberDataController.addKyberData = async (req, res) => {
-//   const kyberData = new KyberData(req.body);
-//   try {
-//     const newKyberData = await kyberData.save();
-//     res.status(201).json(newKyberData);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
+// POST new kyberData
+kyberDataController.addKyberData = async (req, res) => {
+  const kyberData = new KyberData(req.body);
+  try {
+    const newKyberData = await kyberData.save();
+    res.status(201).json(newKyberData);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
-// // PUT update kyberData
-// kyberDataController.updateKyberData = async (req, res) => {
-//   try {
-//     const updatedKyberData = await KyberData.findOneAndUpdate(
-//       { _id: req.params.id },
-//       req.body,
-//       { new: true }
-//     );
-//     if (updatedKyberData) {
-//       res.json(updatedKyberData);
-//     } else {
-//       res.status(404).json({ message: "KyberData not found" });
-//     }
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
+// PUT update kyberData
+kyberDataController.updateKyberData = async (req, res) => {
+  try {
+    const updatedKyberData = await KyberData.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    if (updatedKyberData) {
+      res.json(updatedKyberData);
+    } else {
+      res.status(404).json({ message: "KyberData not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
-// // DELETE kyberData
-// kyberDataController.deleteKyberData = async (req, res) => {
-//   try {
-//     const kyberData = await KyberData.findOne({ _id: req.params.id });
-//     if (kyberData) {
-//       await KyberData.deleteOne({ _id: req.params.id });
-//       res.json({ message: "Deleted kyberData" });
-//     } else {
-//       res.status(404).json({ message: "KyberData not found" });
-//     }
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
+// DELETE kyberData
+kyberDataController.deleteKyberData = async (req, res) => {
+  try {
+    const kyberData = await KyberData.findOne({ _id: req.params.id });
+    if (kyberData) {
+      await KyberData.deleteOne({ _id: req.params.id });
+      res.json({ message: "Deleted kyberData" });
+    } else {
+      res.status(404).json({ message: "KyberData not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports = kyberDataController;
