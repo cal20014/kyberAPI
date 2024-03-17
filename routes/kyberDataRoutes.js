d13a5acf880603dbd8ae4f613a13e8bd1cf5a54d
cal20014 <cal20014@byui.edu>
2024-03-16 20:03:42 -0600

    Update kyberDataRoutes.js and swagger.json

--- routes/kyberDataRoutes.js
+++ routes/kyberDataRoutes.js
@@ -10,16 +10,16 @@ router.get("/", kyberDataController.getAllKyberData);
 // GET single kyberData by id
 router.get("/:id", kyberDataController.getKyberDataById);
 
-// // POST new kyberData
-// router.post("/", utilities.handleErrors(kyberDataController.addKyberData));
+// POST new kyberData
+router.post("/", utilities.handleErrors(kyberDataController.addKyberData));
 
-// // PUT update kyberData
-// router.put("/:id", utilities.handleErrors(kyberDataController.updateKyberData));
+// PUT update kyberData
+router.put("/:id", utilities.handleErrors(kyberDataController.updateKyberData));
 
-// // DELETE kyberData
-// router.delete(
-//   "/:id",
-//   utilities.handleErrors(kyberDataController.deleteKyberData)
-// );
+// DELETE kyberData
+router.delete(
+  "/:id",
+  utilities.handleErrors(kyberDataController.deleteKyberData)
+);
 
 module.exports = router;
